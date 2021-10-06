1d3b331a56ba7fa403cb45a3c14992ad3a52229b
Rkomakhidze1 <rezokomakhidze1@gmail.com>
2021-10-06 18:33:05 +0400

    added text

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -20,28 +20,51 @@ function About() {
     <section className="section-about">
       <div className="u-center-text u-margin-bottom-big">
         <h2 className="heading-secondary" ref={aboutRef}>
-          self taugth software engineer
+          {process.env.REACT_APP_ABOUT_HEADING}
         </h2>
       </div>
       <div className="row">
         <div className="col-1-of-2">
           <h3 className="heading-tertiary u-margin-bottom-small">About me</h3>
-          <p className="paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            enim ab exercitationem, eos sequi mollitia eius iste nihil voluptate
-            deleniti temporibus maxime cupiditate velit tempore. Iure itaque
-            laboriosam illum fugit.
+          <p className="paragraph">{process.env.REACT_APP_ABOUT_ABOUTME}</p>
+          <ul style={{ margin: '-2rem 0 2rem' }}>
+            <li className="paragraph" style={{ margin: 0 }}>
+              GITHUB:
+              <a
+                href="https://github.com/Rkomakhidze1?tab=repositories"
+                style={{ marginLeft: '21px' }}
+              >
+                www.github.com/Rkomakhidze1
+              </a>
+            </li>
+            <li className="paragraph" style={{ margin: 0 }}>
+              EMAIL:
+              <span style={{ marginLeft: '33px' }}>
+                rezokomakhidze1@gmail.com
+              </span>
+            </li>
+            <li className="paragraph" style={{ margin: 0 }}>
+              LINKEDIN:
+              <a
+                href="https://www.linkedin.com/in/rezo-komakhidze-9a9962208/"
+                style={{ marginLeft: '7px' }}
+              >
+                www.linkedin.com/rezo-komakhidze
+              </a>
+            </li>
+            <li className="paragraph" style={{ marginBottom: '2rem' }}>
+              PHONE: <span style={{ marginLeft: '21px' }}>+995 599441919</span>
+            </li>
+          </ul>
+          <h3 className="heading-tertiary u-margin-bottom-small">Experience</h3>
+          <p className="paragraph" style={{ marginBottom: '1rem' }}>
+            {process.env.REACT_APP_ABOUT_EXPERIENCE}
           </p>
-
-          <h3 className="heading-tertiary u-margin-bottom-small">Experince</h3>
           <p className="paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-            asperiores ullam sint possimus maxime eveniet quos quod, cumque
-            iste! Iste alias debitis dolor sequi perferendis deleniti iure ad
-            soluta adipisci.
+            currently preferred tech stack: nodejs + typescript, react +
+            typescript + redux, graphql, postgresql.
           </p>
           <button
-            // href="/#"
             className="btn-text"
             onClick={() => document.dispatchEvent(new ScrollToJobs())}
           >
@@ -53,7 +76,7 @@ function About() {
             style={{
               backgroundSize: 'cover',
               maxWidth: '100%',
-              marginTop: '-3rem',
+              marginTop: '5px',
               borderRadius: '3px',
             }}
             src={image}
